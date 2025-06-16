f2e142c5eb34cdc208fe720fad5da9a9b636a43a
Kenta Suzue <53836169+kentasuzue@users.noreply.github.com>
2025-06-16 15:05:42 -0700

    Update CoinChange.py

--- DynamicProgramming/CoinChange.py
+++ DynamicProgramming/CoinChange.py
@@ -24,7 +24,6 @@ Constraints:
 1 <= coins.length <= 12
 1 <= coins[i] <= 231 - 1
 0 <= amount <= 104
-"""
 
 # dynamic programming with @cache decorator
 class Solution:
@@ -60,3 +59,27 @@ class Solution:
         if amount == 0:
             return 0
         return dp(amount)
+"""
+# iterative solution, which is faster and uses less memory:
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        coins_set = set(coins)
+        coins_sorted = sorted(coins_set, reverse=True)
+        
+        fewest_coins = [float('inf')] * (amount + 1)
+        fewest_coins[0] = 0        
+        
+        for i in range(amount):
+            for coin in coins_sorted:
+                # if i + coin > amount, there will be an error since fewest_coins list has (amount + 1) elements
+                # fewest_coins list elements were initialized to float('inf') so that any finite solution will replace it
+                # the goal is the fewest number of coins, so any lower solution should replace it
+                # fewest_coins[i] + 1 represents fewest_coins[i] + 1 additional coin 
+                if i + coin <= amount and fewest_coins[i] + 1 < fewest_coins[i + coin]:
+                    fewest_coins[i + coin] = fewest_coins[i] + 1
+
+        # if float('inf') then  fewest_coins[amount] was never overwritten so no solution exists
+        if fewest_coins[amount] == float('inf'):
+            return -1
+        else:
+            return fewest_coins[amount]
